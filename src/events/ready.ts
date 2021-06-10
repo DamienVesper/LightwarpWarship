e9c60c6eb6b9c652ff74fc99db2603a6459988a5
Damien Vesper <ldamienvesper@gmail.com>
2021-06-10 16:26:25 -0500

    add ready event

---
+++ src/events/ready.ts
@@ -0,0 +1,17 @@
+import { Client } from '../types/discord';
+import config from '../../config/config';
+
+import log from '../utils/log';
+import { logHeader } from '../utils/logExtra';
+
+import refreshActivity from '../utils/refreshActivity';
+
+export default async (client: Client) => {
+    log(`green`, `Client has started, with ${client.users.cache.size} user(s) in ${client.guilds.cache.size} guild(s).`);
+
+    logHeader(() => {
+        refreshActivity(client, () => {
+            setInterval(() => refreshActivity(client), config.cooldowns.utils.refreshActivity);
+        });
+    });
+};
