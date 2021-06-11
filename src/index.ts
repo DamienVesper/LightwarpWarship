f3d858e55c5925a44a38ab945c692435214a9342
Damien Vesper <ldamienvesper@gmail.com>
2021-06-11 11:13:34 -0500

    HAS to be wrapped in async func

--- src/index.ts
+++ src/index.ts
@@ -20,20 +20,19 @@ process.on(`uncaughtException`, e => log(`red`, e.stack));
 
 // Bot startup.
 const startBot = async () => {
-    logExtra.logSplash(() => {
-        loader.loadCommands(client, () => {
-            loader.loadEvents(client, () => {
+    logExtra.logSplash();
+    loader.loadCommands(client, () => {
+        loader.loadEvents(client, () => {
+            logExtra.logHeader(async () => {
+                await mongoose.connect(process.env.MONGODB_URI, {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true
+                });
+
+                log(`green`, `Connected to database.`);
+
                 logExtra.logHeader(async () => {
-                    await mongoose.connect(process.env.MONGODB_URI, {
-                        useNewUrlParser: true,
-                        useUnifiedTopology: true
-                    });
-
-                    log(`green`, `Connected to database.`);
-
-                    logExtra.logHeader(async () => {
-                        await client.login(process.env.DISCORD_TOKEN).catch(() => log(`red`, `Failed to authenticate client with application.`));
-                    });
+                    await client.login(process.env.DISCORD_TOKEN).catch(() => log(`red`, `Failed to authenticate client with application.`));
                 });
             });
         });
